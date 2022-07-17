acd8555eec5e94b9202df135401f55cad020967d
tsin <chunfeng_tsin@163.com>
2022-07-17 14:53:36 +0800

    04_shader 封装Shader类.

--- 04_shader/main.cpp
+++ 04_shader/main.cpp
@@ -6,6 +6,8 @@
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "Shader.h"
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window)
 {
@@ -65,7 +67,7 @@ int main(){
 
     // 由于我们希望渲染一个三角形，我们一共要指定三个顶点，每个顶点都有一个3D位置。
     // 我们会将它们以标准化设备坐标的形式（OpenGL的可见区域）定义为一个float数组
-    float vertices[] = {  0.5f,  0.5f,    0.0f,      1.0f,  0.3f,  0.5f, // 右上角
+    float vertices[] = {  0.5f,  0.5f,    0.0f,      0.4f,  0.3f,  0.5f, // 右上角
                           0.5f, -0.5f,    0.0f,      0.3f, 0.9f, 1.0f, // 右下角
                         -0.5f, -0.5f,  0.0f,     0.3f, 0.9f, 1.0f, // 左下角
                         -0.5f,  0.5f,  0.0f,     1.0f, 0.3f, 0.5f// 左上角
@@ -77,13 +79,6 @@ int main(){
              1, 2, 3// 第二个三角形
     };
 
-    // 我们通过顶点缓冲对象(Vertex Buffer Objects, VBO)管理这个内存，它会在GPU内存（通常被称为显存）中储存大量顶点。
-    // 使用这些缓冲对象的好处是我们可以一次性的发送一大批数据到显卡上，而不是每个顶点发送一次。从CPU把数据发送到显卡相\
-    // 对较慢，所以只要可能我们都要尝试尽量一次性发送尽可能多的数据。当数据发送至显卡的内存中后，顶点着色器几乎能立即访
-    // 问顶点，这是个非常快的过程。顶点缓冲对象是我们在OpenGL (../01 OpenGL/)教程中第一个出现的OpenGL对象。就像OpenGL
-    // 中的其它对象一样，这个缓冲有一个独一无二的ID，所以我们可以使用glGenBuffers函数和一个缓冲ID生成一个VBO对象
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
 
     //------------------------------------------------------------------------------------------------------
     // 放到bind IBO 前面
@@ -92,208 +87,47 @@ int main(){
     glBindVertexArray(VAO);
     //------------------------------------------------------------------------------------------------------
 
+
     // 添加index buffer
     unsigned int IBO;
     glGenBuffers(1, &IBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO); // 我故意把这个放到前面， 提示你这个是状态机机器, 各个buffer填充的顺序是独立的
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof (indices), indices, GL_STATIC_DRAW);
-    // &&&&&&&&&&&&&&&&&& 但是 你把VAO 放到了后面， 它记录不到你的IBO绑定， 操作失败.....................
 
-
-    //------------------------------------------------------------------------------------------------------
-    // 从后面教程移动过来的
-    // unsigned int VAO;
-    // glGenVertexArrays(1, &VAO);
-    // glBindVertexArray(VAO);
-    //------------------------------------------------------------------------------------------------------
-
-    // OpenGL有很多缓冲对象类型，顶点缓冲对象的缓冲类型是GL_ARRAY_BUFFER。OpenGL允许我们同时绑定多个缓冲，只要它们是不同
-    // 的缓冲类型。我们可以使用glBindBuffer函数把新创建的缓冲绑定到GL_ARRAY_BUFFER目标上, 用一对多的火车轨道去理解：
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    // 这一刻起，我们使用的任何（在GL_ARRAY_BUFFER目标上的）缓冲调用都会用来配置当前绑定的缓冲(VBO)。然后我们可以调用glBufferData函数，
-    // 它会把之前定义的顶点数据复制到缓冲的内存中：
     glBufferData(GL_ARRAY_BUFFER, sizeof (vertices), vertices, GL_STATIC_DRAW);
 
-    // 可以看到，GLSL看起来很像C语言。每个着色器都起始于一个版本声明。OpenGL 3.3以及和更高版本中，
-    // GLSL版本号和OpenGL的版本是匹配的（比如说GLSL 420版本对应于OpenGL 4.2）。我们同样明确表示我们会使用核心模式。
-    // 下一步，使用in关键字，在顶点着色器中声明所有的输入顶点属性(Input Vertex Attribute)。现在我们只关心位置(Position)数据，
-    // 所以我们只需要一个顶点属性。GLSL有一个向量数据类型，它包含1到4个float分量，包含的数量可以从它的后缀数字看出来。
-    // 由于每个顶点都有一个3D坐标，我们就创建一个vec3输入变量aPos。我们同样也通过layout (location = 0)设定了输入变量的位置值
-    // (Location)你后面会看到为什么我们会需要这个位置值
-
-    const char * vertShaderSource = "#version 330 core\n"
-                                    "layout (location = 0) in vec3 aPos;\n"
-                                    "layout (location = 1) in vec3 aColor;\n"
-                                    "out vec3 ourColor;\n"
-                                    "void main(){\n"
-                                    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                    "    ourColor = aColor;\n"
-                                    "}\0";
-
-    // 为了能够让OpenGL使用它，我们必须在运行时动态编译它的源代码。我们首先要做的是创建一个着色器对象，注意还是用ID来引用的。
-    // 所以我们储存这个顶点着色器为unsigned int，然后用glCreateShader创建这个着色器:
-    unsigned int vertShader;
-    vertShader = glCreateShader(GL_VERTEX_SHADER);
-
-    // glShaderSource函数把要编译的着色器对象作为第一个参数。第二参数指定了传递的源码字符串数量，这里只有一个。
-    // 第三个参数是顶点着色器真正的源码，第四个参数我们先设置为NULL。
-    glShaderSource(vertShader, 1, &vertShaderSource, NULL);
-    glCompileShader(vertShader);
-
-    // 如果编译的时候没有检测到任何错误，顶点着色器就被编译成功了。
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &success);
-    if (!success){
-       glGetShaderInfoLog(vertShader, 512, NULL, infoLog);
-       std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
 
 
-    const char *fragShaderSoruce = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "in vec3 ourColor;\n"
-                                   "void main(){    \n"
-                                   "FragColor = vec4(ourColor, 1.0f);\n"
-                                   "}\0";
-    unsigned int fragShader;
-    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragShader, 1, &fragShaderSoruce, NULL);
-    glCompileShader(fragShader);
-
-    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
-    if (!success){
-        glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // 着色器程序对象(Shader Program Object)是多个着色器合并之后并最终链接完成的版本。
-    // 如果要使用刚才编译的着色器我们必须把它们链接(Link)为一个着色器程序对象，然后在渲染对象的时候激活这个着色器程序。
-    // 已激活着色器程序的着色器将在我们发送渲染调用的时候被使用.
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    // glCreateProgram函数创建一个程序，并返回新创建程序对象的ID引用。现在我们需要把之前编译的着色器附加到程序对象上，
-    // 然后用glLinkProgram链接它们
-
-    glAttachShader(shaderProgram, vertShader);
-    glAttachShader(shaderProgram, fragShader);
-    glLinkProgram(shaderProgram);
-
-    // 就像着色器的编译一样，我们也可以检测链接着色器程序是否失败，并获取相应的日志。
-    // 与上面不同，我们不会调用glGetShaderiv和glGetShaderInfoLog，现在我们使用
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success){
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
-    }
+    Shader shader("../04_shader/_vert.glsl", "../04_shader/_frag.glsl");
 
 
-    // 得到的结果就是一个程序对象，我们可以调用glUseProgram函数，用刚创建的程序对象作为它的参数，以激活这个程序对象
-    // 先不使用 glUseProgram(shaderProgram);
-
-    // 对了，在把着色器对象链接到程序对象以后，记得删除着色器对象，我们不再需要它们
-    glDeleteShader(vertShader);
-    glDeleteShader(fragShader);
-
-    // 现在，我们已经把输入顶点数据发送给了GPU，并指示了GPU如何在顶点和片段着色器中处理它。就快要完成了，但还没结束，
-    // OpenGL还不知道它该如何解释内存中的顶点数据，以及它该如何将顶点数据链接到顶点着色器的属性上。我们需要告诉OpenGL
-    // 怎么做。
-
-    // ****** 链接顶点属性 ******
-
-    // 顶点着色器允许我们指定任何以顶点属性为形式的输入。这使其具有很强的灵活性的同时，它还的确意味着我们必须手动指定
-    // 输入数据的哪一个部分对应顶点着色器的哪一个顶点属性。所以，我们必须在渲染前指定OpenGL该如何解释顶点数据。
-
-    // 每个顶点属性从一个VBO管理的内存中获得它的数据，而具体是从哪个VBO（程序中可以有多个VBO）获取则是通过在调用glVertexAttribPointer时
-    // 绑定到GL_ARRAY_BUFFER的VBO决定的。由于在调用glVertexAttribPointer之前绑定的是先前定义的VBO对象，
-    // 顶点属性0现在会链接到它的顶点数据.
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // 现在我们已经定义了OpenGL该如何解释顶点数据，我们现在应该使用glEnableVertexAttribArray，以顶点属性位置值作为参数，
-    // 启用顶点属性；顶点属性默认是禁用的。自此，所有东西都已经设置好了：我们使用一个顶点缓冲对象将顶点数据初始化至缓冲中，
-    // 建立了一个顶点和一个片段着色器，并告诉了OpenGL如何把顶点数据链接到顶点着色器的顶点属性上。在OpenGL中绘制一个物体，
-    // 代码会像是这样 :
-    // 0. 复制顶点数组到缓冲中供OpenGL使用
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // 1. 设置顶点属性指针
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // glEnableVertexAttribArray(0);
-    // 2. 当我们渲染一个物体时要使用着色器程序glUseProgram(shaderProgram);
-    // 3. 绘制物体someOpenGLFunctionThatDrawsOurTriangle()
 
-
-    // 每当我们绘制一个物体的时候都必须重复这一过程。这看起来可能不多，但是如果有超过5个顶点属性，上百个不同物体呢（这其实并不罕见）。
-    // 绑定正确的缓冲对象，为每个物体配置所有顶点属性很快就变成一件麻烦事。有没有一些方法可以使我们把所有这些状态配置储存在一个对象中，
-    // 并且可以通过绑定这个对象来恢复状态呢？
-
-    // OpenGL的核心模式要求我们使用VAO，所以它知道该如何处理我们的顶点输入。如果我们绑定VAO失败，OpenGL会拒绝绘制任何东西
-
-    // 一个顶点数组对象会储存以下这些内容：
-    // • glEnableVertexAttribArray和glDisableVertexAttribArray的调用。
-    // • 通过glVertexAttribPointer设置的顶点属性配置。
-    // • 通过glVertexAttribPointer调用与顶点属性关联的顶点缓冲对象 $ 在这里他是有记录VBO的##################
-
-
-    // unsigned int VAO;
-    // glGenVertexArrays(1, &VAO);
-    // 要想使用VAO，要做的只是使用glBindVertexArray绑定VAO。从绑定之后起，我们应该绑定和配置对应的VBO和属性指针，之后解绑VAO供之后使用。
-    // 当我们打算绘制一个物体的时候，我们只要在绘制物体前简单地把VAO绑定到希望使用的设定上就行了。这段代码应该看起来像这样:
-    // 看PDF ,  然后我们把这快代码写到它应有的地方;
-
-    //跳进while绘制 AAAAAAAAAAAAAAAAAAAAA
-
-    //我们把use program 移出while
-    glUseProgram(shaderProgram);
-    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-    //------------------------------------------------------------------------------------------------------------------
-    //我们可不希望只绘制一个图像之后我们的应用程序就立即退出并关闭窗口。我们希望程序在我们主动关闭它之前不断绘制图像并能够接受用户输入。因此，
-    //我们需要在程序中添加一个while循环，我们可以把它称之为渲染循环(Render Loop)，它能在我们让GLFW退出前一直保持运行。下面几行的代码就实现了一个简单的渲染循环：
+    shader.Use();
     while(!glfwWindowShouldClose(window))
     {
 
-
-
-        //我们同样也希望能够在GLFW中实现一些输入控制，这可以通过使用GLFW的几个输入函数来完成。我们将会使用GLFW的glfwGetKey函数，它需要一个窗口以及一个按键作为输入。
-        // 这个函数将会返回这个按键是否正在被按下。我们将创建一个processInput函数来让所有的输入代码保持整洁。
         processInput(window);
 
         // 渲染指令
-        //为了测试一切都正常工作，我们使用一个自定义的颜色清空屏幕。在每个新的渲染迭代开始的时候我们总是希望清屏，否则我们仍能看见上一次迭代的渲染结果
-        //（这可能是你想要的效果，但通常这不是）。我们可以通过调用glClear函数来清空屏幕的颜色缓冲，它接受一个缓冲位(Buffer Bit)来指定要清空的缓冲，
-        // 可能的缓冲位有GL_COLOR_BUFFER_BIT，GL_DEPTH_BUFFER_BIT和GL_STENCIL_BUFFER_BIT。由于现在我们只关心颜色值，所以我们只清空颜色缓冲。
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-
         //------------------------set uniform---------------
-        float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0, 1.0f);
+        // float timeValue = glfwGetTime();
+        // float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0, 1.0f);
 
-        // 跳入点 AAAAAAAAAAAAAAAAAAAAA
-        // 我们一直期待的三角形
-        // 第二个参数指定了顶点数组的起始索引，我们这里填0。
-        // 最后一个参数指定我们打算绘制多少个顶点，这里是3（我们只从我们的数据中渲染一个三角形，它只3个顶点
         glBindVertexArray(VAO);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-
-
-
-        //glfwPollEvents函数检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）。
-        //glfwSwapBuffers函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
-        //双缓冲(Double Buffer)
-
-        //NOTE:
-        //应用程序使用单缓冲绘图时可能会存在图像闪烁的问题。 这是因为生成的图像不是一下子被绘制出来的，而是按照从左到右，由上而下逐像素地绘制而成的。
-        // 最终图像不是在瞬间显示给用户，而是通过一步一步生成的，这会导致渲染的结果很不真实。为了规避这些问题，我们应用双缓冲渲染窗口应用程序。
-        // 前缓冲保存着最终输出的图像，它会在屏幕上显示；而所有的的渲染指令都会在后缓冲上绘制。当所有的渲染指令执行完毕后，我们交换(Swap)前缓冲和后缓冲，
-        // 这样图像就立即呈显出来，之前提到的不真实感就消除了。
 
         glfwSwapBuffers(window);
         glfwPollEvents();
