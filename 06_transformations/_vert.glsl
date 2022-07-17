78a3e841f97f9828d6ed5e82947a47f9eeab4a00
tsin <chunfeng_tsin@163.com>
2022-07-17 19:19:39 +0800

    06_transformation - 添加glm库   GLM库从0.9.9版本起，默认会将矩阵类型初始化为一个零矩阵（所有元素均为0），而不是单位矩阵（对角元素为1，其它元素为0）。   如果你使用的是0.9.9或0.9.9以上的版本，你需要将所有的矩阵初始化改为glm::mat4 mat = glm::mat4(1.0f)。   如果你想与本教程的代码保持一致，请使用低于0.9.9版本的GLM，或者改用上述代码初始化所有的矩阵,

---
+++ 06_transformations/_vert.glsl
@@ -0,0 +1,19 @@
+#version 330 core
+layout (location = 0) in vec3 aPos; // 位置变量的属性位置值为0
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aUV;
+
+out vec4 ourColor; // 为片段着色器指定一个颜色输出
+out vec2 texCoord;
+
+uniform mat4 transform;
+
+
+void main()
+{
+    gl_Position = transform * vec4(aPos, 1.0); // 注意我们如何把一个vec3作为vec4的构造器的参数
+
+    ourColor = vec4(aColor, 1.0);
+    texCoord = aUV;
+
+}
